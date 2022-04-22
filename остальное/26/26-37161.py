8b8c54db528a6abab167558463fec90082f3cc39
Ziggi <SacharB@yandex.ru>
2022-04-22 20:40:53 +0400

    3

--- остальное/26/26-37161.py
+++ остальное/26/26-37161.py
@@ -1,9 +1,9 @@
 # https://inf-ege.sdamgia.ru/problem?id=37161
 
-# f = open('26-37161.txt')
-f = open('26-test.txt')
+f = open('26-37161.txt')
 f.readline()
 a = []
+otvet = []
 maxR = 0
 minM = 0
 for s in f.read().split('\n'):
@@ -14,43 +14,13 @@ for i in range(len(a)):
     for j in range(i + 1, len(a)):
         if a[i][0] == a[j][0]:
             if a[j][1] - a[i][1] == 3:
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# f = open('26-37161.txt')
-# n = int(f.readline())
-# nums = []
-# for _ in range(n):
-#     pair = list(map(int, f.readline().split()))
-#     pair[1] = -pair[1]
-#     nums += [pair]
-# nums.sort()
-# r, m = 0, 0
-# for i in range(1, len(nums)):
-#     if nums[i][0] == nums[i-1][0]:
-#         if nums[i][1] - nums[i-1][1] == 3:
-#             r = nums[i][0]
-#             m = -nums[i][1] + 1
-#
-# print(r, m)
+                otvet += [a[i]]
+maxa = 0
+minb = 10 ** 20
+for a, b in otvet[::-1]:
+    maxa = max(maxa, a)
+    if a == maxa:
+        minb = min(minb, b)
+    else:
+        break
+print(maxa, minb + 1)
